1cc375fefdb48c098c1fa4c2b104255993db306b
Baghurst <23bbaghurst.acc@lwetb.ie>
2024-10-15 13:42:12 +0100

    Add files via upload

---
+++ PythonFundamentalsEx1.py
@@ -0,0 +1,37 @@
+#1
+
+PI=3.14
+print("Hello user!")
+radius = float(input("Tell me the radius for a circle: "))
+area = PI * radius * radius
+print(f"the area for the circle is {area}")
+
+#2
+
+cm = float(input("How tall are you in cm?: "))
+inches = cm / 2.54
+feet = inches / 12
+print(f"You are {cm} cm, {inches} inches
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
